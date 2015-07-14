99cb993a867f50fac1d9b3df944df79dd2b7fdba
Ryan Walton <ryang24@gmail.com>
2015-07-14 10:23:09 -0700

    added short animation

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+    // log "ready" in console.
+    console.log( "ready!" );
+
+    // short animation to show JQuery in action
+    $( ".fa-user" ).animate({
+        opacity: 0.25
+      }, 2000, function() {
+        // Animation complete
+    });
+
+});
